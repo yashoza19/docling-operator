5f5858651e99cd48c477f4010f8676fc18ac9d31
yashoza19 <yoza@redhat.com>
2025-03-18 14:23:25 -0400

    adding logic to svc and route reconcilers
    Signed-off-by: yashoza19 <yoza@redhat.com>

--- internal/reconcilers/route.go
+++ internal/reconcilers/route.go
@@ -4,8 +4,14 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	routev1 "github.com/openshift/api/route/v1"
 	"github.io/opdev/docling-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -24,6 +30,48 @@ func NewRouteReconciler(client client.Client, log logr.Logger, scheme *runtime.S
 }
 
 func (r *RouteReconciler) Reconcile(ctx context.Context, doclingServ *v1alpha1.DoclingServ) (bool, error) {
+	foundRoute := &routev1.Route{}
+	routeName := doclingServ.Name + "-route"
+
+	err := r.Get(ctx, types.NamespacedName{Name: routeName, Namespace: doclingServ.Namespace}, foundRoute)
+	if err != nil && errors.IsNotFound(err) {
+		r.Log.Info("Creating a new Route", "Route", routeName)
+		route := r.routeForDoclingServ(doclingServ)
+
+		err = r.Create(ctx, route)
+		if err != nil {
+			r.Log.Error(err, "Failed to create new Route", "Route.Namespace", route.Namespace, "Route.Name", route.Name)
+			return true, err
+		}
+	}
 
 	return false, nil
+}
+
+func (r *RouteReconciler) routeForDoclingServ(doclingServ *v1alpha1.DoclingServ) *routev1.Route {
+	labels := labelsForDocling(doclingServ.Name)
+	route := &routev1.Route{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      doclingServ.Name + "-route",
+			Namespace: doclingServ.Namespace,
+			Labels:    labels,
+		},
+		Spec: routev1.RouteSpec{
+			Path: "/", // Pass all traffic
+			To: routev1.RouteTargetReference{
+				Kind: "Service",
+				Name: doclingServ.Name + "-service",
+			},
+			Port: &routev1.RoutePort{
+				TargetPort: intstr.FromString("http"),
+			},
+			TLS: &routev1.TLSConfig{ // Optional: Enable TLS
+				Termination: routev1.TLSTerminationEdge,
+			},
+		},
+	}
+
+	_ = ctrl.SetControllerReference(doclingServ, route, r.Scheme)
+
+	return route
 }
--- internal/reconcilers/service.go
+++ internal/reconcilers/service.go
@@ -3,9 +3,16 @@ package reconcilers
 import (
 	"context"
 
+	ctrl "sigs.k8s.io/controller-runtime"
+
 	"github.com/go-logr/logr"
 	"github.io/opdev/docling-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -24,6 +31,45 @@ func NewServiceReconciler(client client.Client, log logr.Logger, scheme *runtime
 }
 
 func (r *ServiceReconciler) Reconcile(ctx context.Context, doclingServ *v1alpha1.DoclingServ) (bool, error) {
+	foundService := &corev1.Service{}
+	serviceName := doclingServ.Name + "-service"
+
+	err := r.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: doclingServ.Namespace}, foundService)
+	if err != nil && errors.IsNotFound(err) {
+		r.Log.Info("Creating a new Service", "Service", serviceName)
+		service := r.serviceForDoclingServ(doclingServ)
+
+		err = r.Create(ctx, service)
+		if err != nil {
+			r.Log.Error(err, "Failed to create new Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
+			return true, err
+		}
+	}
 
 	return false, nil
+}
+
+func (r *ServiceReconciler) serviceForDoclingServ(doclingServ *v1alpha1.DoclingServ) *corev1.Service {
+	labels := labelsForDocling(doclingServ.Name)
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      doclingServ.Name + "-service",
+			Namespace: doclingServ.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: labels,
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "http",
+					Port:       5001,
+					TargetPort: intstr.FromInt(5001),
+				},
+			},
+		},
+	}
+
+	_ = ctrl.SetControllerReference(doclingServ, service, r.Scheme)
+
+	return service
 }
