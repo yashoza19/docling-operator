54b46cbe403471758184f49c40aa1aedff049c18
yashoza19 <yoza@redhat.com>
2025-03-20 14:20:49 -0400

    test1
    Signed-off-by: yashoza19 <yoza@redhat.com>

--- internal/reconcilers/route.go
+++ internal/reconcilers/route.go
@@ -2,16 +2,16 @@ package reconcilers
 
 import (
 	"context"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/go-logr/logr"
 	routev1 "github.com/openshift/api/route/v1"
 	"github.io/opdev/docling-operator/api/v1alpha1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/intstr"
-	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -30,34 +30,12 @@ func NewRouteReconciler(client client.Client, log logr.Logger, scheme *runtime.S
 }
 
 func (r *RouteReconciler) Reconcile(ctx context.Context, doclingServ *v1alpha1.DoclingServ) (bool, error) {
-	foundRoute := &routev1.Route{}
-	routeName := doclingServ.Name + "-route"
-
-	err := r.Get(ctx, types.NamespacedName{Name: routeName, Namespace: doclingServ.Namespace}, foundRoute)
-	if err != nil && errors.IsNotFound(err) {
-		r.Log.Info("Creating a new Route", "Route", routeName)
-		route := r.routeForDoclingServ(doclingServ)
-
-		err = r.Create(ctx, route)
-		if err != nil {
-			r.Log.Error(err, "Failed to create new Route", "Route.Namespace", route.Namespace, "Route.Name", route.Name)
-			return true, err
-		}
-	}
-
-	return false, nil
-}
-
-func (r *RouteReconciler) routeForDoclingServ(doclingServ *v1alpha1.DoclingServ) *routev1.Route {
-	labels := labelsForDocling(doclingServ.Name)
-	route := &routev1.Route{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      doclingServ.Name + "-route",
-			Namespace: doclingServ.Namespace,
-			Labels:    labels,
-		},
-		Spec: routev1.RouteSpec{
-			Path: "/", // Pass all traffic
+	route := &routev1.Route{ObjectMeta: metav1.ObjectMeta{Name: doclingServ.Name + "-route", Namespace: doclingServ.Namespace}}
+	_, err := controllerutil.CreateOrUpdate(ctx, r.Client, route, func() error {
+		labels := labelsForDocling(doclingServ.Name)
+		route.Labels = labels
+		route.Spec = routev1.RouteSpec{
+			Path: "/",
 			To: routev1.RouteTargetReference{
 				Kind: "Service",
 				Name: doclingServ.Name + "-service",
@@ -65,13 +43,19 @@ func (r *RouteReconciler) routeForDoclingServ(doclingServ *v1alpha1.DoclingServ)
 			Port: &routev1.RoutePort{
 				TargetPort: intstr.FromString("http"),
 			},
-			TLS: &routev1.TLSConfig{ // Optional: Enable TLS
+			TLS: &routev1.TLSConfig{
 				Termination: routev1.TLSTerminationEdge,
 			},
-		},
+		}
+		_ = ctrl.SetControllerReference(doclingServ, route, r.Scheme)
+		return nil
+	})
+
+	if err != nil {
+		r.Log.Error(err, "Error reconciling Route", "Route.Namespace", route.Namespace, "Route.Name", route.Name)
+		return true, err
 	}
 
-	_ = ctrl.SetControllerReference(doclingServ, route, r.Scheme)
-
-	return route
+	r.Log.Info("Successfully reconciled Route", "Route.Namespace", route.Namespace, "Route.Name", route.Name)
+	return false, nil
 }
