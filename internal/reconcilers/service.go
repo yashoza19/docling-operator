7a2627a674ba8764472ede59d9533b9fc91762bd
yashoza19 <yoza@redhat.com>
2025-03-20 14:20:49 -0400

    test1
    Signed-off-by: yashoza19 <yoza@redhat.com>

--- internal/reconcilers/service.go
+++ internal/reconcilers/service.go
@@ -2,16 +2,15 @@ package reconcilers
 
 import (
 	"context"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/go-logr/logr"
 	"github.io/opdev/docling-operator/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -31,45 +30,27 @@ func NewServiceReconciler(client client.Client, log logr.Logger, scheme *runtime
 }
 
 func (r *ServiceReconciler) Reconcile(ctx context.Context, doclingServ *v1alpha1.DoclingServ) (bool, error) {
-	foundService := &corev1.Service{}
-	serviceName := doclingServ.Name + "-service"
-
-	err := r.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: doclingServ.Namespace}, foundService)
-	if err != nil && errors.IsNotFound(err) {
-		r.Log.Info("Creating a new Service", "Service", serviceName)
-		service := r.serviceForDoclingServ(doclingServ)
-
-		err = r.Create(ctx, service)
-		if err != nil {
-			r.Log.Error(err, "Failed to create new Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
-			return true, err
-		}
-	}
-
-	return false, nil
-}
-
-func (r *ServiceReconciler) serviceForDoclingServ(doclingServ *v1alpha1.DoclingServ) *corev1.Service {
-	labels := labelsForDocling(doclingServ.Name)
-	service := &corev1.Service{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      doclingServ.Name + "-service",
-			Namespace: doclingServ.Namespace,
-			Labels:    labels,
-		},
-		Spec: corev1.ServiceSpec{
-			Selector: labels,
-			Ports: []corev1.ServicePort{
-				{
-					Name:       "http",
-					Port:       5001,
-					TargetPort: intstr.FromInt(5001),
-				},
+	service := &corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: doclingServ.Name + "-service", Namespace: doclingServ.Namespace}}
+	_, err := controllerutil.CreateOrUpdate(ctx, r.Client, service, func() error {
+		labels := labelsForDocling(doclingServ.Name)
+		service.Labels = labels
+		service.Spec.Selector = labels
+		service.Spec.Ports = []corev1.ServicePort{
+			{
+				Name:       "http",
+				Port:       5001,
+				TargetPort: intstr.FromInt32(5001),
 			},
-		},
+		}
+		_ = ctrl.SetControllerReference(doclingServ, service, r.Scheme)
+		return nil
+	})
+
+	if err != nil {
+		r.Log.Error(err, "Error reconciling Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
+		return true, err
 	}
 
-	_ = ctrl.SetControllerReference(doclingServ, service, r.Scheme)
-
-	return service
+	r.Log.Info("Successfully reconciled Service", "Service.Namespace", service.Namespace, "Service.Name", service.Name)
+	return false, nil
 }
