c2cff0bcb5c7341bd7d68d11bfa557080abe1db9
Adam D. Cornett <adc@redhat.com>
2025-03-24 13:58:22 -0700

    changing the deployment logic to use controllerutil.CreateOrUpdate
    Signed-off-by: Adam D. Cornett <adc@redhat.com>

--- internal/reconcilers/deployment.go
+++ internal/reconcilers/deployment.go
@@ -7,12 +7,11 @@ import (
 	"github.io/opdev/docling-operator/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 type DeploymentReconciler struct {
@@ -30,64 +29,53 @@ func NewDeploymentReconciler(client client.Client, log logr.Logger, scheme *runt
 }
 
 func (r *DeploymentReconciler) Reconcile(ctx context.Context, doclingServ *v1alpha1.DoclingServ) (bool, error) {
-	foundDeployment := &appsv1.Deployment{}
-	err := r.Get(ctx, types.NamespacedName{Name: doclingServ.Name, Namespace: doclingServ.Namespace}, foundDeployment)
-	if err != nil && errors.IsNotFound(err) {
-		r.Log.Info("Creating a new Deployment", "Deployment", doclingServ.Name)
-		deployment := r.deploymentForDoclingServ(doclingServ)
-
-		err = r.Create(ctx, deployment)
-		if err != nil {
-			r.Log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
-			return true, err
+	deployment := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "docling-serv", Namespace: doclingServ.Namespace}}
+	_, err := controllerutil.CreateOrUpdate(ctx, r.Client, deployment, func() error {
+		labels := labelsForDocling(doclingServ.Name)
+		if deployment.ObjectMeta.CreationTimestamp.IsZero() {
+			deployment.Spec.Selector = &metav1.LabelSelector{
+				MatchLabels: labels,
+			}
 		}
+
+		deployment.Spec.Replicas = &doclingServ.Spec.ReplicaCount
+		deployment.Spec.Template = corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: labels,
+			},
+			Spec: corev1.PodSpec{
+				Containers: []corev1.Container{{
+					Image: doclingServ.Spec.ImageReference,
+					Name:  "docling-serv",
+					Command: []string{
+						"docling-serve",
+						"run",
+					},
+					ImagePullPolicy: corev1.PullIfNotPresent,
+				},
+				},
+			},
+		}
+
+		if doclingServ.Spec.EnableUI {
+			deployment.Spec.Template.Spec.Containers[0].Env = append(deployment.Spec.Template.Spec.Containers[0].Env, []corev1.EnvVar{{
+				Name:  "DOCLING_SERVE_ENABLE_UI",
+				Value: "true",
+			}}...)
+		}
+
+		_ = ctrl.SetControllerReference(doclingServ, deployment, r.Scheme)
+
+		return nil
+	})
+	if err != nil {
+		r.Log.Error(err, "Error reconciling Deployment", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
+		return true, err
 	}
+
+	r.Log.Info("Successfully reconciled Deployment", "Deployment.Namespace", deployment.Namespace, "Deployment.Name", deployment.Name)
 
 	return false, nil
-}
-
-func (r *DeploymentReconciler) deploymentForDoclingServ(doclingServ *v1alpha1.DoclingServ) *appsv1.Deployment {
-	labels := labelsForDocling(doclingServ.Name)
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      doclingServ.Name,
-			Namespace: doclingServ.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &doclingServ.Spec.ReplicaCount,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels,
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: labels,
-				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{{
-						Image: doclingServ.Spec.ImageReference,
-						Name:  "docling-serv",
-						Command: []string{
-							"docling-serve",
-							"run",
-						},
-						ImagePullPolicy: corev1.PullIfNotPresent,
-					},
-					},
-				},
-			},
-		},
-	}
-
-	if doclingServ.Spec.EnableUI {
-		deployment.Spec.Template.Spec.Containers[0].Env = append(deployment.Spec.Template.Spec.Containers[0].Env, []corev1.EnvVar{{
-			Name:  "DOCLING_SERVE_ENABLE_UI",
-			Value: "true",
-		}}...)
-	}
-
-	_ = ctrl.SetControllerReference(doclingServ, deployment, r.Scheme)
-
-	return deployment
 }
 
 func labelsForDocling(name string) map[string]string {
